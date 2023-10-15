556b7e185c7e9348c113fab0713a3c063f9da1a3
sbx_user1051 <sbx_user1051@169.254.6.53>
2023-10-15 12:46:16 +0000

    Sun Oct 15 12:46:16 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,10 +2,18 @@ import collections
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+def validateInput(input) -> bool:
+    if not isinstance(input, str)
 
 def countSKUS(skus) -> dict:
-    count = def
+    items = collections.defaultdict(int)
+
+    for i in skus:
+        items[i]+=1
+
+    return items
 
 def checkout(skus):
     
+
 
