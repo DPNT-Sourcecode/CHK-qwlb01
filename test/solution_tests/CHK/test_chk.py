f2cf809efd12d6ab5273275ad647ad0de8e51801
sbx_user1051 <sbx_user1051@169.254.6.53>
2023-10-15 13:22:16 +0000

    Sun Oct 15 13:22:16 UTC 2023

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,6 +1,16 @@
-from './lib/' import checkout_solution
+import '../../../lib/solutions/CHK/checkout_solution.py' import checkout_solution
 
 
 class TestCHK():
     def test_CHK(self):
         assert checkout_solution.compute("AAA") == 145
+
+
+
+
+
+
+
+tst = TestCHK()
+
+tst.test_CHK()
