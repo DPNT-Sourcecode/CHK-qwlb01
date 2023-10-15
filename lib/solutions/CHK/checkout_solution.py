7d42aa46216848a08e61e7ffa1c5ef18c68f4c6c
sbx_user1051 <sbx_user1051@169.254.6.53>
2023-10-15 14:54:45 +0000

    Sun Oct 15 14:54:45 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -71,11 +71,57 @@ class CalculateShop:
 
         return out
 
+priceTable = """
++------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
+| F    | 10    | 2F get one F free      |
+| G    | 20    |                        |
+| H    | 10    | 5H for 45, 10H for 80  |
+| I    | 35    |                        |
+| J    | 60    |                        |
+| K    | 80    | 2K for 150             |
+| L    | 90    |                        |
+| M    | 15    |                        |
+| N    | 40    | 3N get one M free      |
+| O    | 10    |                        |
+| P    | 50    | 5P for 200             |
+| Q    | 30    | 3Q for 80              |
+| R    | 50    | 3R get one Q free      |
+| S    | 30    |                        |
+| T    | 20    |                        |
+| U    | 40    | 3U get one U free      |
+| V    | 50    | 2V for 90, 3V for 130  |
+| W    | 20    |                        |
+| X    | 90    |                        |
+| Y    | 10    |                        |
+| Z    | 50    |                        |
++------+-------+------------------------+"""
 
-
+def parse(ascii_table):
+    header = []
+    data = []
+    for line in filter(None, ascii_table.split('\n')):
+        if '-+-' in line:
+            continue
+        if not header:
+            header = filter(lambda x: x!='|', line.split())
+            continue
+        data.append(['']*len(header))
+        splitted_line = filter(lambda x: x!='|', line.split())
+        for i in range(len(splitted_line)):
+            data[-1][i]=splitted_line[i]
+    return header, data
 
 def checkout(skus):
 
+    with
+    
     validItems = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10}
     #num offer = for that item, 0 cost = consumable free offer, char = one free of that item
     #odered in terms of best price for customer
