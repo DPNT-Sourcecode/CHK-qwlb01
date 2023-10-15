a97935ec49c6e5e361c00166ef5e3b1dec6b6cd1
sbx_user1051 <sbx_user1051@169.254.6.53>
2023-10-15 13:29:52 +0000

    Sun Oct 15 13:29:52 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -58,6 +58,7 @@ def checkout(skus):
     return calShop.calculateCost()
 
 
+
     
 
 
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -3,4 +3,6 @@ from solutions.CHK import checkout_solution
 
 class TestCHK():
     def test_CHK(self):
-        assert checkout_solution.compute("AAA") == 145
+        assert checkout_solution.compute("AAAABB") == 225
+        assert checkout_solution.compute("AA") == 100
+        assert checkout_solution.compute("AF") == -1
